e8d5b376fff1f26820c37938f420d517f973def8
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-12-16 13:52:48 -0800

    Add comments

--- src/DiffLocs/InputLoop.hs
+++ src/DiffLocs/InputLoop.hs
@@ -16,19 +16,12 @@ import qualified Data.Text.IO         as TIO
 import           System.Exit          (exitFailure)
 import qualified System.IO            as IO
 import qualified System.IO.Error      as IOE
-import           Text.Printf          (HPrintfType, hPrintf)
+import           Text.Printf          (hPrintf)
 
 import           DiffLocs.Diffs
 import           DiffLocs.Types
 
--- Our own version of Maybe to be more explicit around why we're passing what's
--- essentially a Maybe T.Text around.
-data PrevLine
-  = NoPrevLine
-  | PrevLine T.Text
-
-eprintf :: HPrintfType r => String -> r
-eprintf = hPrintf IO.stderr
+----- Printing helpers --------------------------------------------------------
 
 shouldPrintRemoved :: WhichLines -> Bool
 shouldPrintRemoved LinesAdded           = False
@@ -42,8 +35,8 @@ needsPlusOrMinus :: WhichLines -> Bool
 needsPlusOrMinus LinesAdded           = False
 needsPlusOrMinus LinesAddedAndRemoved = True
 
--- Our newtypes ensure that we always match up FromFile and FromLine
--- when printing.
+-- Our newtypes ensure that we always match up FromFile and FromLine when
+-- printing.
 printGoodLineRemoved :: WhichLines -> FromFile -> FromLine -> IO ()
 printGoodLineRemoved whichLines (FromFile filename) (FromLine line) = do
   when (shouldPrintRemoved whichLines) $ do
@@ -51,6 +44,7 @@ printGoodLineRemoved whichLines (FromFile filename) (FromLine line) = do
     TIO.putStr $ prefix <> filename <> ":"
     print line
 
+-- Our newtypes ensure that we always match up ToFile and ToLine when printing.
 printGoodLineAdded :: WhichLines -> ToFile -> ToLine -> IO ()
 printGoodLineAdded whichLines (ToFile filename) (ToLine line) = do
   when (shouldPrintAdded whichLines) $ do
@@ -58,17 +52,7 @@ printGoodLineAdded whichLines (ToFile filename) (ToLine line) = do
     TIO.putStr $ prefix <> filename <> ":"
     print line
 
-succFromLine :: HunkInfo -> HunkInfo
-succFromLine (HunkInfo { hunkFromLine, hunkToLine }) =
-  HunkInfo (succ hunkFromLine) hunkToLine
-
-succToLine :: HunkInfo -> HunkInfo
-succToLine (HunkInfo { hunkFromLine, hunkToLine }) =
-  HunkInfo hunkFromLine (succ hunkToLine)
-
-succBothLines :: HunkInfo -> HunkInfo
-succBothLines (HunkInfo { hunkFromLine, hunkToLine }) =
-  HunkInfo (succ hunkFromLine) (succ hunkToLine)
+----- IO helpers --------------------------------------------------------------
 
 -- Catch the isEOFError exception and turn it into a Nothing.
 -- We need to handle this case specifically and stop looping.
@@ -80,11 +64,35 @@ hTryGetLine handle = do
     Right line -> do
       return $ Just line
 
+-- A little data type to be more explicit about what would effectively be a
+-- Maybe T.Text being threaded through the core input loop.
+--
+-- We need this because sometimes we'll read a line from the handle, realize
+-- that line wasn't meant to be processed by us, and want to indicate that the
+-- parent should process that line instead.
+data PrevLine
+  = NoPrevLine
+  | PrevLine T.Text
+
 hTryGetNextLine :: IO.Handle -> PrevLine -> IO (Maybe T.Text)
 hTryGetNextLine handle prevLine = case prevLine of
   PrevLine line -> return $ Just line
   NoPrevLine    -> hTryGetLine handle
 
+----- Core input loop ---------------------------------------------------------
+
+-- Helpers for incrementing the currently affected line.
+-- We use these to remember which lines we've already seen / processed.
+succFromLine, succToLine, succBothLines :: HunkInfo -> HunkInfo
+succFromLine (HunkInfo { hunkFromLine, hunkToLine }) =
+  HunkInfo (succ hunkFromLine) hunkToLine
+succToLine (HunkInfo { hunkFromLine, hunkToLine }) =
+  HunkInfo hunkFromLine (succ hunkToLine)
+succBothLines (HunkInfo { hunkFromLine, hunkToLine }) =
+  HunkInfo (succ hunkFromLine) (succ hunkToLine)
+
+-- The core decision making. Whether and what to print for a specific hunk of
+-- changed lines.
 processHunk :: Config -> FromFile -> ToFile -> HunkInfo -> IO PrevLine
 processHunk config fileFrom fileTo hunk = do
   let Config fileIn whichLines = config
@@ -104,6 +112,8 @@ processHunk config fileFrom fileTo hunk = do
         | otherwise -> do
           return $ PrevLine line
 
+-- Once we've determined the currently affected files, print all affected lines
+-- for these files.
 processHunks :: Config -> FromFile -> ToFile -> PrevLine -> IO PrevLine
 processHunks config fileFrom fileTo prevLine = do
   let Config fileIn _whichLines = config
@@ -118,6 +128,8 @@ processHunks config fileFrom fileTo prevLine = do
           nextLine <- processHunk config fileFrom fileTo hunk
           processHunks config fileFrom fileTo nextLine
 
+-- Process an entire diff. A diff is grouped by file. For each file, there are
+-- any number of individual hunks representing changes to that file.
 loop :: Config -> PrevLine -> IO ()
 loop config prevLine = do
   let Config fileIn _whichLines = config
@@ -127,7 +139,8 @@ loop config prevLine = do
     Just line -> do
       case parseOnly fromFile line of
         Left _err -> do
-          -- Skip over lines until we find a from line
+          -- Skip over lines until we find a from line Unfortunately, this line
+          -- usually means that parse errors get swallowed.
           loop config NoPrevLine
         Right fileFrom -> do
           hTryGetLine fileIn >>= \case
@@ -137,7 +150,7 @@ loop config prevLine = do
               case parseOnly toFile secondLine of
                 Left err -> do
                   -- Line immediately after fromFile must be toFile
-                  eprintf "Error parsing toFile: %s\n" err
+                  hPrintf IO.stderr "Error parsing toFile: %s\n" err
                   exitFailure
                 Right fileTo -> do
                   nextLine <- processHunks config fileFrom fileTo NoPrevLine
