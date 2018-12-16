3f1b8e9705e414a80203bf642fd708fd77f28dfc
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-12-16 12:53:22 -0800

    Show warning message if stdin is a tty

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
-import qualified System.IO          as IO
+import           Control.Monad         (when)
+import qualified System.IO             as IO
+import           System.Posix.IO       (stdInput)
+import           System.Posix.Terminal (queryTerminal)
 
 import           DiffLocs.InputLoop
 import           DiffLocs.Options
 import           DiffLocs.Types
-import           Paths_diff_locs    (version)
+import           Paths_diff_locs       (version)
 
 main :: IO ()
 main = do
@@ -15,6 +19,10 @@ main = do
   fileIn <- case optionsInput of
     -- Leak the file handle because we're short lived anyways
     InputFromFile filename -> IO.openFile filename IO.ReadMode
-    InputFromStdin         -> return IO.stdin
+    InputFromStdin         -> do
+      isTTY <- queryTerminal stdInput
+      when isTTY $ do
+        IO.hPutStrLn IO.stderr "Warning: reading from stdin, which is a tty."
+      return IO.stdin
 
   run $ Config {configFileIn = fileIn, configWhichLines = optionsWhichLines}
